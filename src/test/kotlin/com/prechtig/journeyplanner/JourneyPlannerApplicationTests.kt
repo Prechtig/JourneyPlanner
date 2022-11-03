40d3fc204e66e6a1ed5e3e7f2757fb71994ff809
Andreas Precht <andreas.precht.poulsen@keylane.com>
2022-11-03 20:28:53 +0100

    Adds simple tests

--- src/test/kotlin/com/prechtig/journeyplanner/JourneyPlannerApplicationTests.kt
+++ src/test/kotlin/com/prechtig/journeyplanner/JourneyPlannerApplicationTests.kt
@@ -1,7 +1,11 @@
 package com.prechtig.journeyplanner
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 
 @SpringBootTest
 class JourneyPlannerApplicationTests {
@@ -10,4 +14,20 @@ class JourneyPlannerApplicationTests {
 	fun contextLoads() {
 	}
 
+}
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+
+	@Test
+	fun testJourneyById() {
+		val entity = restTemplate.getForEntity("/journey/123", Journey::class.java)
+		Assertions.assertEquals(entity.statusCode, HttpStatus.OK)
+	}
+
+	@Test
+	fun testJourneysByUserId() {
+		val entity = restTemplate.getForEntity("/user/123/journeys", List::class.java)
+		Assertions.assertEquals(entity.statusCode, HttpStatus.OK)
+	}
 }
